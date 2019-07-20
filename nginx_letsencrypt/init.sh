e1e51d29b52838b2665ee6a772d87f741701787f
Nick Dieff <nick.dieff@gmail.com>
2019-07-20 14:27:00 +0000

    working letsencrypt

--- nginx_letsencrypt/init.sh
+++ nginx_letsencrypt/init.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-if [ -z $HOST ] || [ -z $EMAIL ] || [ -z $UPSTREAM_SERVER ] then
+# exit oon any failed command
+set -e
+
+if [ -z $HOST ] || [ -z $EMAIL ] || [ -z $UPSTREAM_SERVER ]; then
   echo "Error, missing env var"
   exit 1
 fi
@@ -8,8 +11,9 @@ fi
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/selfsigned.key -out /etc/ssl/selfsigned.crt \
   -subj "/C=US/ST=CA/L=./O=./OU=./CN=$HOST/emailAddress=$EMAIL"
 
-sed -i 's/XXXXXX/$UPSTREAM_SERVER/g' /etc/nginx.conf
-sed -i 's/YYYYYY/$HOST/g' /etc/nginx.conf
-sed -i 's/XXXXXX/$EMAIL/g' /etc/letsencrypt/cli.ini
+sed -i 's!XXXXXX!'$UPSTREAM_SERVER'!g' /etc/nginx/nginx.conf
+sed -i 's!YYYYYY!'$HOST'!g' /etc/nginx/nginx.conf
+sed -i 's!XXXXXX!'$EMAIL'!g' /etc/letsencrypt/cli.ini
 
 supervisord -c /etc/supervisord.conf
+
